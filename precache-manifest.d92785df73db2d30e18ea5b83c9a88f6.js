91318d6150cb28b2505c4a7358cda471d5fed4f3
Iván Roldán Lusich <ivanrl2010@gmail.com>
2020-03-13 00:22:13 -0300

    Updates

---
+++ precache-manifest.d92785df73db2d30e18ea5b83c9a88f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e114a1baee1293007b5e82f90b91db68",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "828cd223e9f8ea602284",
+    "url": "/pomodoro/static/css/main.ba013a29.chunk.css"
+  },
+  {
+    "revision": "c42370719596c8397acc",
+    "url": "/pomodoro/static/js/2.73a1cdbc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pomodoro/static/js/2.73a1cdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828cd223e9f8ea602284",
+    "url": "/pomodoro/static/js/main.39bcc38c.chunk.js"
+  },
+  {
+    "revision": "f7605364f192b19e0c94",
+    "url": "/pomodoro/static/js/runtime-main.a351b351.js"
+  }
+]);
